33768a068e832877987bb9a7892ac08522d33926
Anhkbayar <62504206+Anhkbayar@users.noreply.github.com>
2024-06-07 23:37:17 +0800

    haha

--- Main.py
+++ Main.py
@@ -1,18 +1,17 @@
-# Example file showing a circle moving on screen
+from Tiles import *
 import pygame
 
-# pygame setup
 pygame.init()
-screen = pygame.display.set_mode((800, 400))
+DISPLAY_W, DISPLAY_H = 800, 400
+screen = pygame.display.set_mode((DISPLAY_W, DISPLAY_H))
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+map = TileMap('TestLevel.csv', )
+
 
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
