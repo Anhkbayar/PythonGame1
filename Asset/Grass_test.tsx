1a667363ba648803adbb7f6f8ea1e277cfb44372
Anhkbayar <62504206+Anhkbayar@users.noreply.github.com>
2024-06-08 16:05:29 +0800

    hi

---
+++ Asset/Grass_test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Grass_test" tilewidth="16" tileheight="16" tilecount="10" columns="10">
+ <image source="Grass_test.png" width="160" height="16"/>
+</tileset>
--- Main.py
+++ Main.py
@@ -1,22 +1,33 @@
 from Tiles import *
-import pygame
+from SpriteSheet import Spritesheet
 
 pygame.init()
-DISPLAY_W, DISPLAY_H = 800, 400
-screen = pygame.display.set_mode((DISPLAY_W, DISPLAY_H))
+DISPLAY_W = 800
+DISPLAY_H = int(DISPLAY_W*0.6)
+canvas = pygame.Surface((DISPLAY_W,DISPLAY_H))
+window = pygame.display.set_mode(((DISPLAY_W,DISPLAY_H)))
+pygame.display.set_caption('Niggas in Jungle: 2nd Semester Edition')
+
+x = 200
+y = 200
+scale = 3
+img = pygame.image.load('Asset/Walking_Test.png')
+img = pygame.transform.scale(img, (int(img.get_width()*scale), int(img.get_height()*scale)))
+rect = img.get_rect()
+rect.center = (x,y)
+
 clock = pygame.time.Clock()
+
 running = True
-dt = 0
-
-map = TileMap('TestLevel.csv', )
-
-
 while running:
+    
+    window.blit(img, rect)
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    pygame.display.flip()
-    dt = clock.tick(60) / 1000
-
-pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            pass
+        
+        
+    pygame.display.update()
